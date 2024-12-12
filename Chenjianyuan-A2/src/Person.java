2aa49bc10ce07bb8e4ad3bfa93a6b91e74c5fda8
shan01dian <119046982+shan01dian@users.noreply.github.com>
2024-12-13 03:17:07 +0800

    Update Person.java

--- Chenjianyuan-A2/src/Person.java
+++ Chenjianyuan-A2/src/Person.java
@@ -1,5 +1,38 @@
 public class Person {
     private String name;
     private int age;
-    private
+    private String contact;
+    public Person() {
+        this.name = "";
+        this.age = 0;
+        this.contact = "";
+    }
+
+    public Person(String name, int age, String contact) {
+        this.name = name;
+        this.age = age;
+        this.contact = contact;
+    }
+    public String getName() {
+        return name;
+    }
+    public void setName(String name) {
+        this.name = name;
+    }
+    public int getAge() {
+        return age;
+    }
+    public void setAge(int age) {
+        this.age = age;
+    }
+    public String getContact() {
+        return contact;
+    }
+    public void setContact(String contact) {
+        this.contact = contact;
+    }
+    @Override
+    public String toString() {
+        return "Person(name=" + name + ", age=" + age + ", contact=" + contact + ")";
+    }
 }
