fe245073de3e700c665b7ea59aebe7bca8ab280f
shan01dian <119046982+shan01dian@users.noreply.github.com>
2024-12-14 01:48:38 +0800

    Update RideInterface.java

--- Chenjianyuan-A2/src/RideInterface.java
+++ Chenjianyuan-A2/src/RideInterface.java
@@ -1,2 +1,17 @@
 public interface RideInterface {
+    void addVisitorToQueue(Visitor visitor);
+
+    Visitor removeVisitorFromQueue();
+
+    void printQueue();
+
+    void runOneCycle();
+
+    void addVisitorToHistory(Visitor visitor);
+
+    boolean checkVisitorFromHistory(Visitor visitor);
+
+    int numberOfVisitors();
+
+    void printRideHistory();
 }
