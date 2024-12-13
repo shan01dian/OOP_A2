08052948469cc67a61d806ec77ae6b51abc69665
shan01dian <119046982+shan01dian@users.noreply.github.com>
2024-12-14 01:51:28 +0800

    Update RideInterface.java

--- Chenjianyuan-A2/src/RideInterface.java
+++ Chenjianyuan-A2/src/RideInterface.java
@@ -1,17 +1,17 @@
 public interface RideInterface {
-    void addVisitorToQueue(Visitor visitor);
+    void addVisitorToQueue(Visitor visitor);// 添加游客到队列
 
-    Visitor removeVisitorFromQueue();
+    Visitor removeVisitorFromQueue();// 从队列中移除游客
 
-    void printQueue();
+    void printQueue();//  打印队列
 
-    void runOneCycle();
+    void runOneCycle(); // 运行游乐设施一次循环
 
-    void addVisitorToHistory(Visitor visitor);
+    void addVisitorToHistory(Visitor visitor);//  将游客添加到游玩历史记录
 
-    boolean checkVisitorFromHistory(Visitor visitor);
+    boolean checkVisitorFromHistory(Visitor visitor);//  检查游客是否在历史记录中
 
-    int numberOfVisitors();
+    int numberOfVisitors(); //  获取游玩历史记录中游客的数量
 
-    void printRideHistory();
+    void printRideHistory();//  打印游玩历史记录
 }
