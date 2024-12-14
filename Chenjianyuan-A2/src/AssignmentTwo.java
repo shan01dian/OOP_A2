2aa9f4ca21dce43938434e605832ade320ec5b71
shan01dian <119046982+shan01dian@users.noreply.github.com>
2024-12-14 15:37:02 +0800

    更改Ride类

--- Chenjianyuan-A2/src/AssignmentTwo.java
+++ Chenjianyuan-A2/src/AssignmentTwo.java
@@ -9,17 +9,35 @@ public class AssignmentTwo {
         System.out.println(employee1);
         Employee employee2 = new Employee("nina",18,"12345678","Roller coaster operator","114514");
         System.out.println(employee2);
-        Visitor visitor1 = new Visitor();
+        Visitor visitor0 = new Visitor();
+        System.out.println(visitor0);
+        Visitor visitor1 = new Visitor("anon",20,"1290380933","2024-10-10","Adult");
         System.out.println(visitor1);
-        Visitor visitor2 = new Visitor("anon",20,"1290380933","2024-10-10","Adult");
+        Visitor visitor2 = new Visitor("momok",18,"1290380234","2024-10-10","Child");
         System.out.println(visitor2);
+        Visitor visitor3 = new Visitor("soyo",16,"1145141919","2024-10-11","Child");
+        System.out.println(visitor3);
+        Visitor visitor4 = new Visitor("tomo",21,"1293429044","2024-10-11","Adult");
+        System.out.println(visitor4);
+        Visitor visitor5 = new Visitor("tomotin",22,"1243242344","2024-10-11","Adult");
         Ride ride1 = new Ride("Roller Coaster", true, employee2);
         System.out.println(ride1);
         Ride ride2 = new Ride("Ferris Wheel", false, null);
         System.out.println(ride2);
+        AssignmentTwo assignment = new AssignmentTwo();
+        assignment.partThree(ride1, visitor1, visitor2, visitor3, visitor4, visitor5);
     }
-    public void partThree(){
+
+    private void partThree(Ride ride, Visitor visitor1, Visitor visitor2, Visitor visitor3, Visitor visitor4, Visitor visitor5) {
+        System.out.println("\n Part3, Queue: " + ride.getRideName()+"\n");
+        ride.addVisitorToQueue(visitor1);
+        ride.addVisitorToQueue(visitor2);
+        ride.addVisitorToQueue(visitor3);
+        ride.addVisitorToQueue(visitor4);
+        ride.addVisitorToQueue(visitor5);//添加所有游客到队列
     }
+
+
     public void partFourA(){
     }
     public void partFourB(){
--- Chenjianyuan-A2/src/Ride.java
+++ Chenjianyuan-A2/src/Ride.java
@@ -1,17 +1,23 @@
-public class Ride {
+import java.util.LinkedList;
+import java.util.Queue;
+
+public class Ride implements RideInterface{
     private String rideName;//设备名称
     private boolean isOpen;//布尔值判断设施是否开启
     private Employee operator;//Employee类型，操作员
+    private Queue<Visitor> waiting;//等待队列
 
     public Ride(){
         this.rideName = "";
         this.isOpen = false;
         this.operator = null;
+        this.waiting = new LinkedList<>();
     }
     public Ride(String rideName, boolean isOpen, Employee operator){
         this.rideName = rideName;
         this.isOpen = isOpen;
         this.operator = operator;
+        this.waiting = new LinkedList<>();//初始化队列
     }
     public String getRideName(){
         return this.rideName;
@@ -31,6 +37,67 @@ public class Ride {
     public void setOperator(Employee operator){
         this.operator = operator;
     }
+    @Override
+    public void addVisitorToQueue(Visitor visitor){
+        if(visitor==null){
+            System.out.println("Unable to add null number of visitors");
+            return;
+        }
+        waiting.add(visitor);
+        System.out.println(visitor.getName()+" is added"+rideName+" to waiting");
+    }//添加游客到队列
+
+    @Override
+    public Visitor removeVisitorFromQueue() {
+        if(waiting.isEmpty()){
+            System.out.println("Unable to remove null visitor");
+            return null;
+        }
+        Visitor removedVisitor = waiting.poll();
+        System.out.println(removedVisitor.getName()+"Has been removed from the wait queue for" + rideName + ".");
+        return removedVisitor;
+    }//从队列中删除游客
+
+    @Override
+    public void printQueue() {
+        if(waiting.isEmpty()){
+            System.out.println("Cannot print if queue is null");
+            return;
+        }
+        System.out.println(rideName + "Current wait queue:");
+        int position=0;
+        for(Visitor visitor : waiting){
+            System.out.println(//visitor.getName()
+                     +position+":"+visitor);
+            position++;
+        }
+    }//打印等待序列
+
+    @Override
+    public void runOneCycle() {
+
+    }
+
+    @Override
+    public void addVisitorToHistory(Visitor visitor) {
+
+    }
+
+    @Override
+    public boolean checkVisitorFromHistory(Visitor visitor) {
+        return false;
+    }
+
+    @Override
+    public int numberOfVisitors() {
+        return 0;
+    }
+
+    @Override
+    public void printRideHistory() {
+
+    }
+
     @Override
     public String toString(){
         String operatorInfo = (this.operator != null) ? this.operator.toString() : "No operator";
